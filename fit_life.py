417b25892e9f015ca306f35b37a7f53bc407ee9b
Amina Alekseevna <amialekseevna@gmail.com>
2026-09-19 08:29:54 +0300

    исправила ошибки

--- fit_life.py
+++ fit_life.py
@@ -12,21 +12,17 @@ def main():
     user_name = input("Как вас зовут? ")
     user_age = input("Сколько вам лет? ")
 
-
     weight_input = input("Ваш вес в кг: ")
     user_weight = float(weight_input.replace(',', '.'))
     height_input = input("Ваш рост в метрах: ")
     user_height = float(height_input.replace(',', '.'))
-
 
     bmi = user_weight / (user_height ** 2)
     round_bmi = round(bmi, DECIMAL_PLACES)
     water_ml = user_weight * WATER_PER_KG
     water_l = water_ml / ML_IN_LITER
 
-
     return user_name, user_age, round_bmi, water_l
-
 
 if __name__ == '__main__':
     name, age, bmi_val, water_val = main()
