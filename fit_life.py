1f39d1564dba7e47a9fb841c77d82f31f0fb879c
Amina Alekseevna <amialekseevna@gmail.com>
2026-09-19 08:14:16 +0300

    исправила

--- fit_life.py
+++ fit_life.py
@@ -7,34 +7,33 @@ DECIMAL_PLACES = 1
 
 # 1. Знакомство
 def main():
-    """Запускает бота:собирает данные, считает ИМТ и норму воды,отчет"""
+    """Запускает бота:собирает данные, считает ИМТ и норму воды,отчет."""
     print("Здравствуйте! Я фитнес-бот для расчета ИМТ")
     user_name = input("Как вас зовут? ")
     age_input = input("Сколько вам лет? ")
     user_age = int(float(age_input.replace(',', '.')))
 
 
-# 2. Сбор данных
+    # 2. Сбор данных
     weight_input = input("Ваш вес в кг: ")
     user_weight = float(weight_input.replace(',', '.'))
     height_input = input("Ваш рост в метрах: ")
-    user_height = float(height_input.replace(',', '.'))
 
 
-# 3. Логика расчетов
+    # 3. Логика расчетов
     bmi = user_weight / (user_height ** 2)
     round_bmi = round(bmi, DECIMAL_PLACES)
     water_ml = user_weight * WATER_PER_KG
     water_l = water_ml / ML_IN_LITER
 
 
-# 4. Вывод красивого результата
+    # 4. Вывод красивого результата
     return user_name, user_age, round_bmi, water_l
 
 
 if __name__ == '__main__':
     name, age, bmi_val, water_val = main()
-print(
-    f"Отчет для пользователя: {name}, {age} лет\n"
-    f"Твой Индекс Массы Тела: {bmi_val}\n"
-    f"Рекомендуемая норма воды: {water_val} л в день")
+    print(
+        f"Отчет для пользователя: {name}, {age} лет\n"
+        f"Твой Индекс Массы Тела: {bmi_val}\n"
+        f"Рекомендуемая норма воды: {water_val} л в день" )
